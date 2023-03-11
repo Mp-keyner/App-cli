844a60ef328974a28849f57bae0306688c09495f
mp-keyner <keyneroswaldo0@gmail.com>
2023-03-11 15:36:50 -0500

    mostrar info de la api

--- src/hook/useApi.js
+++ src/hook/useApi.js
@@ -1,50 +1,6 @@
 import { useState } from "react";
 
-let urlApiCli = 'https://api.openweathermap.org/data/2.5/weather?appid=26c25a94171b06cf681d940caed738cf&lang=es'
-let cityUrl = "&q="
-let urlForecast = 'https://api.openweathermap.org/data/2.5/weather?appid=26c25a94171b06cf681d940caed738cf&lang=es'
-
-const [weather, setweather] = useState([])
-const [forecast, setForecast] = useState([])
-const [show, setShow] = useState(false)
-const [location, setLocation] = useState("")
-const [loading, setloading] = useState(false)
 
 
-const getLocation = async(loc) => {
-    setloading(true)
-    setLocation(loc)
 
-    // /weather/weather
-    urlApiCli = urlApiCli +   cityUrl + loc
-
-    await fetch(urlApiCli).then((response)=> {
-      if (!response.ok) throw {response}
-        return response.json()
-    }).then((weatherData) => {
-        setweather(weatherData)
-        console.log(weatherData, 'Actual')
-    } ).catch(error =>{
-      console.log(error)
-      setloading(false)
-      setShow(false)
-    })
-
-    urlForecast = urlForecast + cityUrl + loc
-
-    await fetch(urlForecast).then((response)=> {
-      if (!response.ok) throw {response}
-        return response.json()
-    }).then((forecastData) => {
-        setForecast(forecastData)
-        console.log(forecastData, 'Futuro')
-        setloading(false)
-        setShow(true)
-    } ).catch(error =>{
-      console.log(error)
-      setloading(false)
-      setShow(false)
-    })
-
-}
 export default getLocation
--- src/screens/HomreScreen.jsx
+++ src/screens/HomreScreen.jsx
@@ -4,22 +4,81 @@ import {
   Text,
   TouchableOpacity,
   TextInput,
+  Image
 } from "react-native";
 import React, { useState } from "react";
-import {} from '../hook/useApi'
+import Card from "../components/Card";
 
 const HomreScreen = () => {
   const [value, setvalue] = useState("");
 
+  let urlApiCli =
+    "https://api.openweathermap.org/data/2.5/weather?appid=26c25a94171b06cf681d940caed738cf&lang=es";
+  let cityUrl = "&q=";
+  let urlForecast =
+    "https://api.openweathermap.org/data/2.5/weather?appid=26c25a94171b06cf681d940caed738cf&lang=es";
 
+  const [weather, setweather] = useState([]);
+  const [forecast, setForecast] = useState([]);
+  const [show, setShow] = useState(false);
+  const [location, setLocation] = useState("");
+  const [loading, setloading] = useState(false);
 
+  const getLocation = async (loc) => {
+    setloading(true);
+    setLocation(loc);
 
-  const {getLocation} = useApi()  
+    // /weather/weather
+    urlApiCli = urlApiCli + cityUrl + loc;
+
+    await fetch(urlApiCli)
+      .then((response) => {
+        if (!response.ok) throw { response };
+        return response.json();
+      })
+      .then((weatherData) => {
+        setweather(weatherData);
+        console.log(weatherData, "Actual");
+      })
+      .catch((error) => {
+        console.log(error);
+        setloading(false);
+        setShow(false);
+      });
+
+    urlForecast = urlForecast + cityUrl + loc;
+
+    await fetch(urlForecast)
+      .then((response) => {
+        if (!response.ok) throw { response };
+        return response.json();
+      })
+      .then((forecastData) => {
+        setForecast(forecastData);
+        console.log(forecastData, "Futuro");
+        setloading(false);
+        setShow(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        setloading(false);
+        setShow(false);
+      });
+
+    return {
+      setLocation,
+      setloading,
+      setForecast,
+      setShow,
+      setweather,
+    };
+  };
+  
   const buscar = () => {
-    console.log(value)
-    setvalue('')
-    getLocation(value)  
-  }
+    console.log(value);
+    setvalue("");
+    getLocation(value);
+  };
 
   return (
     <>
@@ -38,8 +97,14 @@ const HomreScreen = () => {
           </TouchableOpacity>
         </View>
         <View>
-          <Text style={{textAlign: 'center'}}>hello como ta tu </Text>
-        </View>
+          <Text style={{ textAlign: "center" }}>hello como ta tu </Text>
+        </View> 
+        <Card
+          show={show}
+          loading={loading}
+          weather={weather}
+          forecast={forecast}
+        />
       </View>
     </>
   );
